37ca1a0d6f05ddf6d98440e97db23b4c432a347d
Edward Ruzek <edadiddles@hotmail.com>
2024-11-30 12:24:17 -0500

    writing correlation id to connection

--- app/server.go
+++ app/server.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -19,9 +15,15 @@ func main() {
 	    fmt.Println("Failed to bind to port 9092")
 	    os.Exit(1)
 	}
-	_, err = l.Accept()
+    conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 	    os.Exit(1)
 	}
+    
+    message_size := []byte{0, 0, 0, 0}
+    correlation_id := []byte{0, 0, 0, 7}
+    conn.Write(message_size)
+    conn.Write(correlation_id)
 }
+
