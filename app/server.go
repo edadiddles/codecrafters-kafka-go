62e448b395a67f03f7cdad0e6552e3d0aff56766
Edward Ruzek <edadiddles@hotmail.com>
2024-12-01 06:39:00 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+    "bytes"
 )
 
 func main() {
@@ -23,9 +24,31 @@ func main() {
     buff := make([]byte, 1024)
     conn.Read(buff)
     // msg = 4, req_api_key = 2, req_api_vers = 2, correlation_id = 4
+    request_api_version := buff[6:8]
     correlation_id := buff[8:12]
     message_size := []byte{0, 0, 0, 0}
+    supported_api_versions := [][]byte{
+        {0, 0}, // version 0
+        {0, 1}, // version 1
+        {0, 2}, // version 2
+        {0, 3}, // version 3
+        {0, 4}, // version 4
+    }
+    is_api_version_supported := false
+    for i := 0; i < len(supported_api_versions); i++ {
+        if bytes.Equal(request_api_version, supported_api_versions[i]) {
+            is_api_version_supported = true
+        }
+    }
+    
+    // capture error
+    error_code := []byte{0,0}
+    if !is_api_version_supported {
+        error_code = []byte{0, 35}
+    }
+
     conn.Write(message_size)
     conn.Write(correlation_id)
+    conn.Write(error_code)
 }
 
